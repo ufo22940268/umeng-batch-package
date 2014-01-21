09a75d5078b6225966f0bd9ee96ea784bc0058c0
ufo22940268 <ufo22940268@gmail.com>
2014-01-21 09:33:15 +0800

    up

---
+++ app/scripts/select.js
@@ -0,0 +1,14 @@
+$("#btnLeft").click(function () {
+    var selectedItem = $("#rightValues option:selected");
+    $("#leftValues").append(selectedItem);
+});
+
+$("#btnRight").click(function () {
+    var selectedItem = $("#leftValues option:selected");
+    $("#rightValues").append(selectedItem);
+});
+
+$("#rightValues").change(function () {
+    var selectedItem = $("#rightValues option:selected");
+    $("#txtRight").val(selectedItem.text());
+});
